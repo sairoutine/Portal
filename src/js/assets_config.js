66875365fc3a4ce4e5417377e10518b911c0e30e
sairoutine <sairoutine@gmail.com>
2017-11-15 23:49:36 +0900

    add: loading scene

---
+++ src/js/assets_config.js
@@ -0,0 +1,13 @@
+var AssetsConfig = {};
+AssetsConfig.images = {
+	//"black_mist.png":            "./image/black_mist.png",
+};
+
+AssetsConfig.sounds = {
+};
+
+AssetsConfig.bgms = {
+};
+
+
+module.exports = AssetsConfig;
--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,8 @@
 'use strict';
 var core = require('./hakurei').core;
 var util = require('./hakurei').util;
+var SceneLoading = require('./hakurei').scene.loading;
+var AssetsConfig = require('./assets_config');
 
 var Game = function(canvas) {
 	core.apply(this, arguments);
@@ -9,6 +11,10 @@ util.inherit(Game, core);
 
 Game.prototype.init = function () {
 	core.prototype.init.apply(this, arguments);
+
+	this.addScene("loading", new SceneLoading(this));
+
+	this.changeScene("loading", AssetsConfig);
 };
 
 module.exports = Game;
