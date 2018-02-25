48efede09eed4c6cc31a2eaa72c934f074932615
sairoutine <sairoutine@gmail.com>
2018-02-25 11:29:01 +0900

    update: require scenes

--- src/js/game.js
+++ src/js/game.js
@@ -1,7 +1,10 @@
 'use strict';
 var core = require('./hakurei').core;
 var util = require('./hakurei').util;
-var SceneTitle = require('./scene/title');
+var SceneTitle        = require('./scene/title');
+var SceneMatching     = require('./scene/matching');
+var SceneBattle       = require('./scene/battle');
+var SceneBattleResult = require('./scene/battle_result');
 var AssetsConfig = require('./assets_config');
 
 var Game = function(canvas) {
@@ -12,7 +15,10 @@ util.inherit(Game, core);
 Game.prototype.init = function () {
 	core.prototype.init.apply(this, arguments);
 
-	this.addScene("title", new SceneTitle(this));
+	this.addScene("title",         new SceneTitle(this));
+	this.addScene("matching",      new SceneMatching(this));
+	this.addScene("battle",        new SceneBattle(this));
+	this.addScene("battle_result", new SceneBattleResult(this));
 
 	this.changeScene("loading", AssetsConfig, "title");
 };
